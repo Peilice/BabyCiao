e26b963bc81bd1fc874c327f1b94e23b88d1491b
Alice <feather1989@hotmail.com>
2024-06-21 21:38:51 +0800

    240621

--- BabyCiao/Program.cs
+++ BabyCiao/Program.cs
@@ -1,4 +1,5 @@
 using BabyCiao.Data;
+using BabyCiao.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,8 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDbContext<BabyCiaoContext>(options => options.UseSqlServer("Babyciao"));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
