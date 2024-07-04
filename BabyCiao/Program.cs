4654fda8426db7aa68e6cdb6190413f383e3d195
陳毅強 <@MSI>
2024-07-04 09:48:25 +0800

    1111

--- BabyCiao/Program.cs
+++ BabyCiao/Program.cs
@@ -18,6 +18,13 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,7 +35,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -37,11 +43,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 app.MapRazorPages();
 
 app.Run();
---
+++ BabyCiao/ViewModel/UserRegistrationViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabyCiao.ViewModels
+{
+    public class UserRegistrationViewModel
+    {
+        [Required(ErrorMessage = "請輸入用戶名")]
+        public string Account { get; set; }
+
+        [Required(ErrorMessage = "請輸入電子郵件地址")]
+        [EmailAddress(ErrorMessage = "電子郵件地址無效")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "請輸入密碼")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "請同意條款和政策")]
+        public bool Agree { get; set; }
+    }
+}
