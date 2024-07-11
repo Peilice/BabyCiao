b8cebc95a89702112d2cf317d9f141cd216038d1
Alice <feather1989@hotmail.com>
2024-07-11 17:04:08 +0800

    0711 API

---
+++ BabyCiaoAPI/Models/Platform.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BabyCiaoAPI.Models;
+
+public partial class Platform
+{
+    public int Id { get; set; }
+
+    public string AccountUserAccount { get; set; } = null!;
+
+    public DateOnly ModifiedTime { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Content { get; set; } = null!;
+
+    public string Type { get; set; } = null!;
+
+    public bool Display { get; set; }
+
+    public virtual UserAccount AccountUserAccountNavigation { get; set; } = null!;
+
+    public virtual ICollection<PlatformPhoto> PlatformPhotos { get; set; } = new List<PlatformPhoto>();
+
+    public virtual ICollection<PlatformResponse> PlatformResponses { get; set; } = new List<PlatformResponse>();
+}
