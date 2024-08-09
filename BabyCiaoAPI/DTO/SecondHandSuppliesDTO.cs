c2b8ab71bce4889315616c40ff91043a47bc4868
Lily <lilyweioao@gmail.com>
2024-08-09 11:50:24 +0800

    二手商品新增、刪除ok

--- BabyCiaoAPI/DTO/SecondHandSuppliesDTO.cs
+++ BabyCiaoAPI/DTO/SecondHandSuppliesDTO.cs
@@ -77,7 +77,35 @@ namespace BabyCiaoAPI.DTO
 		//public List<SuppliesPhotoDTO>? Photos { get; set; }
 		//public List<IFormFile>? PhotoFiles { get; set; }//上傳檔案
 	}
-        public class SuppliesPhotoDTO
+    public class SecondHandDetailDTO
+    {
+        [Display(Name = "商品編號")]
+        public int Id { get; set; }
+
+        [Display(Name = "上傳用戶")]
+        public string AccountUserAccount { get; set; } = null!;
+
+        [Display(Name = "商品名稱")]
+        public string SuppliesName { get; set; } = null!;
+
+        [Display(Name = "商品敘述")]
+        public string SuppliesDescription { get; set; } = null!;
+
+        [Display(Name = "庫存")]
+        public int StockQuantity { get; set; }
+
+
+        [Display(Name = "上傳時間")]
+        public string? ModifiedTimeView { get; set; }
+
+        [Display(Name = "類別")]
+        public string Type { get; set; } = null!;
+
+
+
+		public List<SuppliesPhotoDTO>? Photos { get; set; }
+    }
+    public class SuppliesPhotoDTO
 	{
 		public int Id { get; set; }
 
