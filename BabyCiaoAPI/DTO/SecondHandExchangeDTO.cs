2f8b43f6ebbbae7e4bda198e2305108526263137
Lily <lilyweioao@gmail.com>
2024-08-10 08:54:16 +0800

    二手商品編輯完成

---
+++ BabyCiaoAPI/DTO/SecondHandExchangeDTO.cs
@@ -0,0 +1,33 @@
+﻿namespace BabyCiaoAPI.DTO
+{
+    public class SecondHandExchangeDTO
+    {
+        public int Id { get; set; }
+
+        public string BuyerId { get; set; } = null!;//申請者
+
+        public string SellerId { get; set; } = null!;//賣家
+
+        public int WantGetId { get; set; }//申請者想要的物品
+
+        public int GetQuantity { get; set; }//申請者想要的物品數量
+
+        public int WantGiveId { get; set; }//申請者要給賣家的
+
+        public int GiveQuantity { get; set; }//申請者要給賣家的數量
+
+        public DateTime ModifiedTime { get; set; }//異動時間
+
+        public string Statement { get; set; } = null!;//申請交換單狀態(申請中、確認成交、不成立)
+
+
+    }
+    public class SecondHandExchangeOrderDTO
+    {
+        public int IdExchangeOrder { get; set; }
+
+        public bool BuyerOk { get; set; }
+
+        public bool SellerOk { get; set; }
+    }
+}
