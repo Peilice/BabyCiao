965991c53504d138c5cba645cfc7b0248bdbd052
陳毅強 <@MSI>
2024-08-14 12:54:03 +0800

    8/14

---
+++ BabyCiaoAPI/DTO/LoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BabyCiaoAPI.DTO
+{
+    public class LoginDTO
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public int Permissions { get; set; }
+    }
+}
