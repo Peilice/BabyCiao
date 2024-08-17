78c8069c9f309bae0f6336217c431bcf28d78751
陳毅強 <@MSI>
2024-08-17 23:50:56 +0800

    數據分析內容添加

--- BabyCiao/Controllers/DataAnalysisController.cs
+++ BabyCiao/Controllers/DataAnalysisController.cs
@@ -20,32 +20,79 @@ namespace BabyCiao.Controllers
         public async Task<IActionResult> Index()
         {
             // 抓取會員數的總筆數
-            var parentTotalCount = await _context.UserAccounts
-                .CountAsync();
+            var parentTotalCount = await _context.UserAccounts.CountAsync();
 
             // 抓取聯絡簿的總筆數
-            var nannyTotalCount = await _context.ContactBooks
-                .CountAsync();
+            var nannyTotalCount = await _context.ContactBooks.CountAsync();
 
-            var AddressTotalCount = await _context.UserInformations
-               .CountAsync();
+            // 定義需要顯示的縣市列表
+            var cities = new List<string>
+            {
+                "台北市", "新北市", "桃園市", "台中市", "台南市", "高雄市",
+                "新竹縣", "新竹市", "苗栗縣", "彰化縣", "南投縣", "雲林縣",
+                "嘉義縣", "嘉義市", "屏東縣", "宜蘭縣", "花蓮縣", "台東縣"
+            };
+
+            // 抓取全台各縣市會員的總筆數
+            var addressTotalCount = await _context.UserInformations
+                .Where(u => cities.Any(city => u.Address.StartsWith(city)))
+                .GroupBy(u => cities.FirstOrDefault(city => u.Address.StartsWith(city)))
+                .Select(g => new AddressCountViewModel
+                {
+                    Address = g.Key,
+                    Count = g.Count()
+                })
+                .ToListAsync();
+
+            // 確保所有縣市都有數據
+            var addressTotalCountFinal = cities
+                .Select(city => new AddressCountViewModel
+                {
+                    Address = city,
+                    Count = addressTotalCount.FirstOrDefault(a => a.Address == city)?.Count ?? 0
+                })
+                .ToList();
+
+            // 抓取各競賽名稱的總筆數
+            var competitionNameCount = await _context.OnlineCompetitions
+                .GroupBy(c => c.CompetitionName)
+                .Select(g => new CompetitionNameCountViewModel
+                {
+                    CompetitionName = g.Key,
+                    Count = g.Count()
+                })
+                .ToListAsync();
 
             // 準備數據發送到視圖
             var model = new DataAnalysisViewModel
             {
                 ParentTotalCount = parentTotalCount,
                 NannyTotalCount = nannyTotalCount,
-                AddressTotalCount = AddressTotalCount
+                AddressTotalCount = addressTotalCountFinal,
+                CompetitionNameCount = competitionNameCount
             };
 
             return View(model);
         }
     }
 
+    public class AddressCountViewModel
+    {
+        public string Address { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class CompetitionNameCountViewModel
+    {
+        public string CompetitionName { get; set; }
+        public int Count { get; set; }
+    }
+
     public class DataAnalysisViewModel
     {
         public int ParentTotalCount { get; set; }
         public int NannyTotalCount { get; set; }
-        public int AddressTotalCount { get; set; }
+        public List<AddressCountViewModel> AddressTotalCount { get; set; }
+        public List<CompetitionNameCountViewModel> CompetitionNameCount { get; set; }
     }
 }
